2d8ef20ea1dee10bffa796a4d9a2fa136d8d5c46
senyonyo <t16438as@sfc.keio.ac.jp>
2017-06-26 23:58:11 +0900

    追加

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import tweepy
 import secret
 import codecs
 import MySQLdb
+import time
+import datetime
+
+connector = MySQLdb.connect(host='localhost',port=3306,user="root",passwd='Densuke3!',db='tweets_collection',charset='utf8')
+cursor = connector.cursor()
 
 CK= secret.twkey['ck']
 CS= secret.twkey['cs']
@@ -15,24 +20,33 @@ auth = tweepy.OAuthHandler(CK, CS)
 auth.set_access_token(ATK, ATS)
 api = tweepy.API(auth_handler=auth)
 
-#api.update_status("Hello, world!")
 keyword = "lang:ja min_retweets:50"
 
 def search_rest(api,keyword,search_count=10):
   search_result = api.search(keyword,count=search_count)
   for i,twt in enumerate(search_result):
-    #print"---%3d---" % (i + 1)
-    #print twt.created_at
-    #print twt.user.name
-    #print twt.text +"\n"
-    
-    with open('tweet.txt','a') as f:
-      f.write("---"+str(i+1) +"---\n")
-      f.write(str(twt.created_at).encode('utf-8')+"\n")
-      f.write(twt.user.name.encode('utf-8')+"\n")
-      f.write(twt.text.encode('utf-8')+"\n")
-      f.write("rt:"+str(twt.retweet_count).encode('utf-8')+"\n")
-      f.write("fav:"+str(twt.favorite_count).encode('utf-8')+"\n")
-      f.write("\n")
+    print (int(time.mktime(twt.created_at.timetuple())),twt.user.name.encode('utf8'),int(twt.user.id),twt.text.encode('utf8'),int(twt.id),int(twt.retweet_count),int(twt.favorite_count))
+    cursor.execute(
+      'insert into tweets_table'
+      '(date,username,userid,text,textid,rt,fav)'
+      'VALUES (%s,%s,%s,%s,%s,%s,%s)',
+      (int(time.mktime(twt.created_at.timetuple())),twt.user.name.encode('utf8'),int(twt.user.id),twt.text.encode('utf8'),int(twt.id),int(twt.retweet_count),int(twt.favorite_count))
+    )
+    connector.commit()
+
+    cursor.close()
+  
+  connector.close()
+ 
+#   with open('tweet.txt','a') as f:
+#     f.write("---"+str(i+1) +"---\n")
+#     f.write(str(twt.created_at).encode('utf-8')+"\n")
+#     f.write(twt.user.name.encode('utf-8')+"\n")
+#     f.write(twt.text.encode('utf-8')+"\n")
+#     f.write("rt:"+str(twt.retweet_count).encode('utf-8')+"\n")
+#     f.write("fav:"+str(twt.favorite_count).encode('utf-8')+"\n")
+#     f.write("\n")
 
 search_rest(api,keyword,30)
+
+
