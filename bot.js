594cf2ad2a6929379f479f51b80d05dd2e827653
senyonyo <t16438as@sfc.keio.ac.jp>
2017-07-04 10:43:15 +0900

    完成版追加

--- bot.js
+++ bot.js
@@ -7,26 +7,29 @@ var bot  = new twitter({
 	access_token_secret : process.env.ACCESS_TOKEN_SECRET 
 });
 
-bot.post('statuses/update',
+/*bot.post('statuses/update',
 	{status: '半ライス大盛(このツイートはテストです)'},
 	function(error, tweet, response) {
 		if (error) {
 			console.error(error);
 		}
   })
+*/
 
 function searchTweet(){
   bot.get('search/tweets', { q: 'lang:ja min_retweets:50' }, 
     function(err, data, response) {
-      var time = new Date().getgetTime();
+      var time = new Date().getTime();
       var fs = require('fs');
-      console.log(JSON.stringify(data));
-      fs.writeFile(time+'.json',JSON.stringfy(data),function(err){
+      //console.log(JSON.stringify(data));
+      //console.log(data);
+      var json_data = JSON.stringify(data);
+      fs.writeFile('outputs/'+time+'.json',json_data,function(err){
         console.log(err);
       })
     }
   );
 }
 
-
-setInterval(searchTweet, 2*60*1000);
+searchTweet();
+setInterval(searchTweet, 120*60*1000);
